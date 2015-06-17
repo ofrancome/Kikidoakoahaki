9c5d9386f8cac8c8d0991fbb60e210b68bf26c98
ofrancome <olivierfrancome@gmail.com>
2015-06-17 08:52:01 -0700

    Revert "Importing Src"
    This reverts commit bf9dd82d4368379cc49a0a2d43be88d9662c7217.

--- src/connectDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$bdd = new PDO('mysql:host='. $DB_HOST .';dbname=' . $DB_NAME, $DB_USER, $DB_PASSWORD);
-$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
--- src/credentials/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-$DB_HOST		= 'db559453258.db.1and1.com';
-$DB_NAME		= 'db559453258';
-$DB_USER 		= 'dbo559453258';
-$DB_PASSWORD 	= 'KdK2015.';
-?>
