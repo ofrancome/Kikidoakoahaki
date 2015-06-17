bf9dd82d4368379cc49a0a2d43be88d9662c7217
ofrancome <olivierfrancome@gmail.com>
2015-06-17 08:51:15 -0700

    Importing Src
    First import of existing src

---
+++ src/connectDB.php
@@ -0,0 +1,6 @@
+<?php
+
+$bdd = new PDO('mysql:host='. $DB_HOST .';dbname=' . $DB_NAME, $DB_USER, $DB_PASSWORD);
+$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ src/credentials/credentials.php
@@ -0,0 +1,6 @@
+<?php
+$DB_HOST		= 'db559453258.db.1and1.com';
+$DB_NAME		= 'db559453258';
+$DB_USER 		= 'dbo559453258';
+$DB_PASSWORD 	= 'KdK2015.';
+?>
